8a0f615e432f9af97812be4dad0fe4822ca48c48
Egor Pugin <egor.pugin@gmail.com>
2018-12-16 00:22:03 +0300

    Throw usual runtime_error from command.

--- src/command/src/command.cpp
+++ src/command/src/command.cpp
@@ -468,7 +468,7 @@ void Command::execute1(std::error_code *ec_in)
         return;
     }
 
-    throw SW_RUNTIME_EXCEPTION(getError());
+    throw std::runtime_error(getError());
 }
 
 String Command::getError() const
