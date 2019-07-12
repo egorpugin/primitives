ece9a775c5bc43e13f2866044b569b890ece4db6
Egor Pugin <egor.pugin@gmail.com>
2019-07-12 15:26:51 +0300

    Initial smtp email via curl.

---
+++ src/http/include/primitives/email.h
@@ -0,0 +1,33 @@
+#include <primitives/string.h>
+
+namespace primitives
+{
+
+struct Person
+{
+    String name;
+    String email;
+};
+
+struct Email
+{
+    Person to;
+    std::vector<Person> cc;
+    Person from;
+    String title;
+    String body;
+
+    bool custom_email = false;
+};
+
+struct Smtp
+{
+    String server;
+    String user;
+    String password;
+
+    String sendEmail(const Email &);
+    void setAuth(const String &);
+};
+
+}
