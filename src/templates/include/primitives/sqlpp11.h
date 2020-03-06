d9238cb76ed40a8debef23fd7c2ff351bbaf19fd
Egor Pugin <egor.pugin@gmail.com>
2020-03-06 14:24:51 +0300

    Pass unique ptr from sqlpp11 scope to prevent early callback calls.

--- src/templates/include/primitives/sqlpp11.h
+++ src/templates/include/primitives/sqlpp11.h
@@ -6,19 +6,19 @@
 #include <sqlpp11/sqlite3/connection.h>
 
 [[nodiscard]]
-primitives::ExtendedScopeExit sqlpp11_transaction(::sqlpp::sqlite3::connection &c)
+std::unique_ptr<primitives::ExtendedScopeExit> sqlpp11_transaction(::sqlpp::sqlite3::connection &c)
 {
-    primitives::ExtendedScopeExit ese([&c] {c.start_transaction(); });
-    ese.on_error = [&c] {c.rollback_transaction(false); };
-    ese.on_success = [&c] {c.commit_transaction(); };
-    return std::move(ese); // move is not needed for cpp17
+    auto ese = std::make_unique<primitives::ExtendedScopeExit>([&c] {c.start_transaction(); });
+    ese->on_error = [&c] {c.rollback_transaction(false); };
+    ese->on_success = [&c] {c.commit_transaction(); };
+    return ese;
 }
 
 [[nodiscard]]
-primitives::ExtendedScopeExit sqlpp11_transaction_manual(::sqlpp::sqlite3::connection &c)
+std::unique_ptr<primitives::ExtendedScopeExit> sqlpp11_transaction_manual(::sqlpp::sqlite3::connection &c)
 {
-    primitives::ExtendedScopeExit ese([&c] {c.execute("BEGIN"); });
-    ese.on_error = [&c] {c.execute("ROLLBACK"); };
-    ese.on_success = [&c] {c.execute("COMMIT"); };
-    return std::move(ese); // move is not needed for cpp17
+    auto ese = std::make_unique<primitives::ExtendedScopeExit>([&c] {c.execute("BEGIN"); });
+    ese->on_error = [&c] {c.execute("ROLLBACK"); };
+    ese->on_success = [&c] {c.execute("COMMIT"); };
+    return ese;
 }
