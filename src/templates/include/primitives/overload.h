e630f1d841574475dc33cb6aa9a1cc87f7c79c0a
Egor Pugin <egor.pugin@gmail.com>
2021-04-09 15:53:27 +0300

    [templates] C++20 overload.

--- src/templates/include/primitives/overload.h
+++ src/templates/include/primitives/overload.h
@@ -1,30 +1,8 @@
 #pragma once
 
-// lambda overloads
-template <class... Fs> struct overload_set;
-
-template <class F1, class... Fs>
-struct overload_set<F1, Fs...> : F1, overload_set<Fs...>::type
-{
-    typedef overload_set type;
-
-    overload_set(F1 head, Fs... tail)
-        : F1(head), overload_set<Fs...>::type(tail...)
-    {}
-
-    using F1::operator();
-    using overload_set<Fs...>::type::operator();
+template<typename... Ts>
+struct overload : Ts... {
+    overload(Ts... ts) : Ts(ts)... {}
+    using Ts::operator()...;
 };
-
-template <class F>
-struct overload_set<F> : F
-{
-    typedef F type;
-    using F::operator();
-};
-
-template <class... Fs>
-typename overload_set<Fs...>::type overload(Fs... x)
-{
-    return overload_set<Fs...>(x...);
-}
+//template<class... Ts> overload(Ts...) -> overload<Ts...>;
