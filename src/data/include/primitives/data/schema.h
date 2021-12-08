f956d93da43bcc9a57eb370ff079c8f8031bce08
Egor Pugin <egor.pugin@gmail.com>
2021-12-09 02:53:34 +0300

    [data] Misc.

--- src/data/include/primitives/data/schema.h
+++ src/data/include/primitives/data/schema.h
@@ -1,23 +1,8 @@
 #pragma once
 
+#include "string.h"
+
 #include <boost/hana.hpp>
-
-#include <string>
-
-#define TEXT_FIELD(x)                                                                                                  \
-    struct x##_ {                                                                                                      \
-        char x[64];                                                                                                    \
-        constexpr x##_(const char *n) : x() {                                                                          \
-            for (int i = 0; *n; i++)                                                                                   \
-                x[i] = *n++;                                                                                           \
-        }                                                                                                              \
-        constexpr auto operator<=>(const x##_ &) const = default;                                                      \
-        operator std::string() const { return &x[0]; }                                                                 \
-    };                                                                                                                 \
-    template <x##_ v>                                                                                                  \
-    constexpr x##_ operator""_##x() {                                                                                  \
-        return v;                                                                                                      \
-    }
 
 namespace primitives::data::schema {
 
---
+++ src/data/include/primitives/data/string.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+#define TEXT_FIELD(x)                                                                                                  \
+    struct x##_ {                                                                                                      \
+        char x[64];                                                                                                    \
+        constexpr x##_(const char *n) : x() {                                                                          \
+            for (int i = 0; *n; i++)                                                                                   \
+                x[i] = *n++;                                                                                           \
+        }                                                                                                              \
+        constexpr auto operator<=>(const x##_ &) const = default;                                                      \
+        operator std::string() const { return &x[0]; }                                                                 \
+    };                                                                                                                 \
+    template <x##_ v>                                                                                                  \
+    constexpr x##_ operator""_##x() {                                                                                  \
+        return v;                                                                                                      \
+    }
