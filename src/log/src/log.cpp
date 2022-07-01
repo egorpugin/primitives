cebc2a02781bcc82f4f2401dff05fa28a2f1d1bf
Egor Pugin <egor.pugin@gmail.com>
2022-07-01 21:48:37 +0300

    [log] Fix rotation.

--- src/log/src/log.cpp
+++ src/log/src/log.cpp
@@ -108,7 +108,7 @@ void initLogger(LoggerSettings &s)
             {
                 auto backend = boost::make_shared<tfb>
                     (
-                        boost::log::keywords::file_name = s.log_file + ".log." + s.log_level + ".txt",
+                        boost::log::keywords::file_name = s.log_file + ".log." + s.log_level + ".%N.txt",
                         boost::log::keywords::rotation_size = 10 * 1024 * 1024,
                         boost::log::keywords::open_mode = open_mode//,
                                                                    //boost::log::keywords::auto_flush = true
@@ -130,7 +130,7 @@ void initLogger(LoggerSettings &s)
                 {
                     auto backend = boost::make_shared<tfb>
                         (
-                            boost::log::keywords::file_name = s.log_file + ".log." + name + ".txt",
+                            boost::log::keywords::file_name = s.log_file + ".log." + name + ".%N.txt",
                             boost::log::keywords::rotation_size = 10 * 1024 * 1024,
                             // always append to trace, do not recreate
                             boost::log::keywords::open_mode = app_mode//,
