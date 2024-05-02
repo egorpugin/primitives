5ccddcfb642922f2a340f0c98a46d217571bb7a9
Egor Pugin <egor.pugin@gmail.com>
2024-05-02 18:25:00 +0300

    [type_name] Allow to get it with namespaces.

--- src/templates2/include/primitives/templates2/type_name.h
+++ src/templates2/include/primitives/templates2/type_name.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <source_location>
+#include <string_view>
+using namespace std::literals;
+
 template <typename T>
-consteval auto type_name() {
+consteval auto type_name_with_namespaces() {
     std::string_view fn = std::source_location::current().function_name();
     auto structstr = "struct "sv;
     auto classstr = "class "sv;
 #ifdef _MSC_VER
     fn = fn.substr(fn.rfind("<") + 1);
-    fn = fn.substr(fn.rfind(":") + 1);
     if (fn.starts_with(structstr)) {
         fn = fn.substr(structstr.size());
     } else if (fn.starts_with(classstr)) {
@@ -19,7 +22,13 @@ consteval auto type_name() {
 #else
     fn = fn.substr(fn.rfind("T = ") + 4);
     fn = fn.substr(0, fn.size() - 1);
-    fn = fn.substr(fn.rfind(":") + 1); // remove namespaces
 #endif
+    return fn;
+}
+
+template <typename T>
+consteval auto type_name() {
+    auto fn = type_name_with_namespaces<T>();
+    fn = fn.substr(fn.rfind(":") + 1); // remove namespaces
     return fn;
 }
