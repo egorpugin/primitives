ba7c7ee8a2486c2e02e7db1f042725d9c1656331
Egor Pugin <egor.pugin@gmail.com>
2024-06-01 22:50:22 +0300

    Add static string.

--- src/templates2/include/primitives/templates2/string.h
+++ src/templates2/include/primitives/templates2/string.h
@@ -3,6 +3,22 @@
 #include <string>
 #include <vector>
 
+template<std::size_t N>
+struct static_string {
+    char p[N]{};
+
+    constexpr static_string(char const(&pp)[N]) {
+        std::ranges::copy(pp, p);
+    }
+    //operator auto() const { return &p[0]; }
+    constexpr auto begin() const {return p;}
+    constexpr auto end() const {return p+size();}
+    constexpr auto operator[](int i) const {return p[i];}
+    static constexpr auto size() {return N-1;}
+};
+template<static_string s>
+constexpr auto operator "" _s() { return s; }
+
 auto find_text_between(auto &&text, auto &&from, auto &&to) {
     std::vector<std::string> fragments;
     auto pos = text.find(from);
