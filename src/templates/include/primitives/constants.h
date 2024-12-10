13eb628b4dc1ed1c73f6fe04e8919233b52e5d73
Egor Pugin <egor.pugin@gmail.com>
2024-12-10 16:58:21 +0300

    Misc.

--- src/templates/include/primitives/constants.h
+++ src/templates/include/primitives/constants.h
@@ -1,31 +1,31 @@
 #pragma once
 
-constexpr auto operator "" _B(unsigned long long int i)
+constexpr auto operator ""_B(unsigned long long int i)
 {
     return i;
 }
 
-constexpr auto operator "" _KB(unsigned long long int i)
+constexpr auto operator ""_KB(unsigned long long int i)
 {
     return i * 1024_B;
 }
 
-constexpr auto operator "" _MB(unsigned long long int i)
+constexpr auto operator ""_MB(unsigned long long int i)
 {
     return i * 1024_KB;
 }
 
-constexpr auto operator "" _GB(unsigned long long int i)
+constexpr auto operator ""_GB(unsigned long long int i)
 {
     return i * 1024_MB;
 }
 
-constexpr auto operator "" _TB(unsigned long long int i)
+constexpr auto operator ""_TB(unsigned long long int i)
 {
     return i * 1024_GB;
 }
 
-constexpr auto operator "" _PB(unsigned long long int i)
+constexpr auto operator ""_PB(unsigned long long int i)
 {
     return i * 1024_TB;
 }
