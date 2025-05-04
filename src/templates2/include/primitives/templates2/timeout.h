78a712f7e5d183fd378d429e88237657ecc0302a
Egor Pugin <egor.pugin@gmail.com>
2025-05-04 23:32:26 +0300

    Update.

--- src/templates2/include/primitives/templates2/timeout.h
+++ src/templates2/include/primitives/templates2/timeout.h
@@ -13,57 +13,60 @@ void sleep(auto &&d) {
     std::this_thread::sleep_for(d);
 }
 
+struct timeout {
+    std::chrono::duration<float> t;
+    std::chrono::duration<float> max;
+    std::chrono::duration<float> step;
+    std::chrono::duration<float> exception_timeout;
+    std::chrono::duration<float> total;
+
+    int n{ 0 }; // can't make lambda because we need n access, need we?
+
+    explicit operator bool() const { return total > exception_timeout; }
+    void sleep(auto &&t) {
+        total += t;
+        std::this_thread::sleep_for(t);
+        if (*this) {
+            throw timeout_exception{ std::format("timeout: {}", total) };
+        }
+    }
+    auto operator()() {
+        simple_wait();
+        t += step;
+        t = std::min(max, t);
+        ++n;
+        return operator bool();
+    }
+    void simple_wait() {
+        sleep(t);
+    }
+};
+
 inline auto make_timeout(
     std::chrono::duration<float> start = 10ms,
     std::chrono::duration<float> max = 100ms,
     std::chrono::duration<float> step = 10ms,
     std::chrono::duration<float> exception_timeout = 20s) {
-    struct timeout {
-        std::chrono::duration<float> t;
-        std::chrono::duration<float> max;
-        std::chrono::duration<float> step;
-        std::chrono::duration<float> exception_timeout;
-        std::chrono::duration<float> total;
-
-        int n{0}; // can't make lambda because we need n access, need we?
-
-        explicit operator bool() const { return total > exception_timeout; }
-        void sleep(auto &&t) {
-            total += t;
-            std::this_thread::sleep_for(t);
-            if (*this) {
-                throw timeout_exception{std::format("timeout: {}", total)};
-            }
-        }
-        auto operator()() {
-            simple_wait();
-            t += step;
-            t = std::min(max, t);
-            ++n;
-            return operator bool();
-        }
-        void simple_wait() {
-            sleep(t);
-        }
-    };
     return timeout{start, max, step, exception_timeout};
 }
 
 template <typename Exception>
-auto while_no_exception_with_timeout(auto &&f, auto && ... args) {
+auto while_no_exception_with_timeout(auto &&f, auto &&log, auto && ... args) {
     auto timeout = make_timeout(args...);
     while (1) {
         try {
             return f();
         }
         catch (Exception &e) {
-            LOG_WARN(logger, e.what());
+            //LOG_WARN(logger, e.what());
+            //std::cerr << e.what() << "\n"sv;
+            log(e.what());
             timeout();
         }
     }
 }
-auto while_no_exception_with_timeout(auto &&f, auto && ... args) {
-    return while_no_exception_with_timeout<std::exception>(f, args...);
+auto while_no_exception_with_timeout(auto &&f, auto &&log, auto && ... args) {
+    return while_no_exception_with_timeout<std::exception>(f, log, args...);
 }
 
 } // namespace primitives::templates
